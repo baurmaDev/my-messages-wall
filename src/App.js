674ad53a56f86f4a172492eedf010c49e7f82aa8
chizi56 <bauka1215@gmail.com>
2022-06-23 17:12:57 +0600

    feat: add connectwallet function

--- src/App.js
+++ src/App.js
@@ -25,9 +25,26 @@ const App = () => {
     }
 
   }
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const check = () => {
     if(!currentAccount){
-      return <button>Connect Wallet</button>;
+      return <button onClick={connectWallet}>Connect Wallet</button>;
     }else{
       return <Navigate to="/main" replace={true} />
     }
